15d59d5a9a811dad0cc6f8b759f8d92199073578
phantomlord <genocyber.dex@gmail.com>
2022-07-07 18:23:04 -0500

    segundo commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React :v lol
+          Learn React :v
         </a>
       </header>
     </div>
