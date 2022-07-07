d99e6448c4fe8f2bb2457c39fb7714424039a0a9
phantomlord <genocyber.dex@gmail.com>
2022-07-07 18:03:07 -0500

    se agrego lol

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React :v
+          Learn React :v lol
         </a>
       </header>
     </div>
